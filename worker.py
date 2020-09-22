dc0096e18af18946fc7cd63d85bfb7cdba51f23c
Vadim G <rrretry@ya.ru>
2020-09-23 04:23:24 +0500

    clean text before request images

--- worker.py
+++ worker.py
@@ -1,18 +1,21 @@
+import spacy
 from rq import Worker as Base
 from yandex_images_download.downloader import YandexImagesDownloader, get_driver
 
 
 downloader: YandexImagesDownloader = None
 driver = None
+nlp = None
 
 
 class Worker(Base):
     def __init__(self, queues=None, *args, **kwargs):
         super().__init__(queues, *args, **kwargs)
-        global downloader, driver
+        global downloader, driver, nlp
+        nlp = spacy.load('ru2')
         driver = get_driver('Firefox', path="./geckodriver")
         downloader = YandexImagesDownloader(driver=driver, output_directory='./static/downloads',
-                                            limit=3, exact_isize=[1080, 1920])
+                                            limit=4, exact_isize=[1080, 1920])
 
     def request_stop(self, *args, **kwargs):
         global driver
@@ -20,13 +23,25 @@ class Worker(Base):
         super().request_stop(*args, **kwargs)
 
 
+def clean_text(text):
+    global nlp
+    pos_tags = ['NOUN', 'PROPN', 'ADJ']
+    cleaned = []
+    for token in nlp(text):
+        if token.pos_ in pos_tags and not token.is_stop:
+            cleaned.append(token.text)
+    if len(cleaned) > 0:
+        return " ".join(cleaned)
+    else:
+        return text
+
+
 def download_images_task(text):
     global downloader
-    results = downloader.download_images([text])
+    results = downloader.download_images([clean_text(text)])
     files = []
     for image in results.keyword_results:
-        if image.keyword == text:
+        if len(image.keyword) > 0:
             for page in image.page_results:
                 files.extend([r.img_path for r in page.img_url_results])
     return text, files
-
